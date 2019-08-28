4974b57000ec622c091ca7ac4ae415fc505f9514
yashu51 <54583612+yashu51@users.noreply.github.com>
2019-08-28 18:14:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,11 @@
+public class Hello {
 
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = 5 + 11;
+        System.out.println(myFirstNumber);
+
+    }
+}
